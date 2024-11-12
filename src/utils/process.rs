14400949674a1e1dcd2ea33d5b74571c87e94019
Farioso Fernando <farioso.f@gmail.com>
2024-11-12 04:05:23 +0200

    feat: list and recovery

--- src/utils/process.rs
+++ src/utils/process.rs
@@ -1,8 +1,11 @@
+use chrono::NaiveDateTime;
 use std::fs::{self, File};
 use std::io::Write;
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
 use std::process::Command;
+use std::time::SystemTime;
 
+use super::args_struct::Args;
 use crate::utils::path::get_bkp_path;
 
 fn get_pid_file_path() -> PathBuf {
@@ -92,7 +95,6 @@ pub fn get_process_uptime(pid: u32) -> String {
             start_time_str = start_time_str.replace(*pt, *en);
         }
 
-        // Tentar fazer o parsing
         let start_time =
             chrono::NaiveDateTime::parse_from_str(&start_time_str.trim(), "%a %b %d %H:%M:%S %Y");
 
@@ -112,4 +114,156 @@ pub fn get_process_uptime(pid: u32) -> String {
     } else {
         "Não foi possível calcular o tempo.".to_string()
     }
+}
+
+pub fn apply_backup(backup_file: &str, args: &Args) {
+    let copy_output = Command::new("docker")
+        .args(&[
+            "cp",
+            get_bkp_path()
+                .join(backup_file)
+                .to_str()
+                .expect("Falha ao obter o caminho do arquivo de backup"),
+            &format!(
+                "{}:/tmp/{}",
+                args.container
+                    .clone()
+                    .expect("Necessário prover nome do container"),
+                backup_file
+            ),
+        ])
+        .output()
+        .expect("Falha ao copiar o arquivo de backup para o contêiner");
+
+    if !copy_output.status.success() {
+        eprintln!(
+            "Erro ao copiar o arquivo para o contêiner: {:?}",
+            String::from_utf8_lossy(&copy_output.stderr)
+        );
+        return;
+    }
+
+    let output = Command::new("docker")
+        .args(&[
+            "exec",
+            &args
+                .container
+                .clone()
+                .expect("Necessário prover nome do container"),
+            "psql",
+            "-U",
+            &args
+                .user
+                .clone()
+                .expect("Necessário prover nome do usuário"),
+            "-d",
+            &args
+                .database
+                .clone()
+                .expect("Necessário prover nome do banco"),
+            "-f",
+            &format!("/tmp/{}", backup_file), // isto deve usar o caminho do proprio container.
+        ])
+        .output()
+        .expect("Falha ao executar o comando psql");
+
+    if output.status.success() {
+        println!("Backup restaurado com sucesso!");
+
+        let log_file = get_bkp_path().join(".purrs");
+        let mut log = fs::OpenOptions::new()
+            .append(true)
+            .create(true)
+            .open(log_file)
+            .expect("Falha ao abrir o arquivo de log");
+
+        let now = chrono::Local::now();
+        writeln!(
+            log,
+            "Backup restaurado de: {} - Data: {}",
+            backup_file,
+            now.format("%d/%m/%Y %H:%M")
+        )
+        .expect("Falha ao escrever no log");
+    } else {
+        eprintln!(
+            "Erro ao restaurar o backup: {:?}",
+            String::from_utf8_lossy(&output.stderr)
+        );
+    }
+}
+
+pub fn list_backups(tool_path: &Path) {
+    let entries = fs::read_dir(tool_path).expect("Falha ao ler o diretório de backups");
+
+    let mut backups = vec![];
+    let last_restored = read_last_restored();
+
+    for entry in entries {
+        if let Ok(entry) = entry {
+            let path = entry.path();
+            if path.is_file() && path.extension() == Some(std::ffi::OsStr::new("sql")) {
+                let metadata = fs::metadata(&path).expect("Erro ao obter metadados do arquivo");
+                let created = metadata.created().expect("Erro ao obter data de criação");
+
+                if let Ok(duration_since_epoch) = created.duration_since(SystemTime::UNIX_EPOCH) {
+                    let created_naive = NaiveDateTime::from_timestamp(
+                        duration_since_epoch.as_secs() as i64,
+                        duration_since_epoch.subsec_nanos(),
+                    );
+
+                    let formatted_time = created_naive.format("%d/%m/%Y %H:%M").to_string();
+
+                    if let Some(file_name) = path.file_name() {
+                        if let Some(file_str) = file_name.to_str() {
+                            let is_last_restored = if let Some(last_backup) = &last_restored {
+                                file_str == last_backup
+                            } else {
+                                false
+                            };
+                            backups.push((file_str.to_string(), formatted_time, is_last_restored));
+                        }
+                    }
+                } else {
+                    eprintln!(
+                        "Erro ao calcular a duração desde a época para o arquivo: {}",
+                        path.display()
+                    );
+                }
+            }
+        }
+    }
+
+    println!("======================================================================");
+    println!(
+        "{:<27} | {:<20} | {:<20}",
+        "   ARQUIVOS", "   DATA", "ÚLTIMO RESTAURADO"
+    );
+    println!("======================================================================");
+
+    for (file, time, is_last_restored) in backups {
+        let last_restored_label = if is_last_restored { "SIM" } else { "NÃO" };
+        println!("{:<20} | {:<20} | {:<20}", file, time, last_restored_label);
+    }
+
+    println!("======================================================================");
+}
+
+fn read_last_restored() -> Option<String> {
+    let log_file_path = get_bkp_path().join(".purrs");
+
+    if let Ok(contents) = fs::read_to_string(log_file_path) {
+        let last_line = contents.lines().last();
+
+        if let Some(last_entry) = last_line {
+            let parts: Vec<&str> = last_entry.split_whitespace().collect();
+            if parts.len() >= 4 {
+                return Some(parts[3].to_string());
+            }
+        }
+    }
+
+    println!("Foi impossível abrir o arquivo dos purrs");
+
+    None
 }
