9e33a39b47b902eb11097d8684f61abd6a52bddc
Farioso Fernando <farioso.f@gmail.com>
2024-12-11 09:36:50 +0200

    ref: fix some warnings

--- src/utils/schedule.rs
+++ src/utils/schedule.rs
@@ -1,27 +1,27 @@
 use std::time::Duration;
 
 pub enum Schedule {
-    OneMonth,
-    SevenDays,
-    SixDays,
+    _OneMonth,
+    _SevenDays,
+    _SixDays,
     OneDay,
-    TwelveHours,
-    OneHour,
-    ThirtyMinutes,
-    OneMinute,
+    _TwelveHours,
+    _OneHour,
+    _ThirtyMinutes,
+    _OneMinute,
 }
 
 impl Schedule {
     pub fn to_duration(&self) -> Duration {
         match self {
-            Schedule::OneMonth => Duration::from_secs(30 * 24 * 60 * 60), // 1 mes
-            Schedule::SevenDays => Duration::from_secs(7 * 24 * 60 * 60), // 7 dias
-            Schedule::SixDays => Duration::from_secs(6 * 24 * 60 * 60),   // 6 dias
-            Schedule::OneDay => Duration::from_secs(24 * 60 * 60),        // 1 dia
-            Schedule::TwelveHours => Duration::from_secs(12 * 60 * 60),   // 12 horas
-            Schedule::OneHour => Duration::from_secs(60 * 60),            // 1 hora
-            Schedule::ThirtyMinutes => Duration::from_secs(30 * 60),      // 30 min
-            Schedule::OneMinute => Duration::from_secs(60),               // 1 min
+            Schedule::_OneMonth => Duration::from_secs(30 * 24 * 60 * 60), // 1 mes
+            Schedule::_SevenDays => Duration::from_secs(7 * 24 * 60 * 60), // 7 dias
+            Schedule::_SixDays => Duration::from_secs(6 * 24 * 60 * 60),   // 6 dias
+            Schedule::OneDay => Duration::from_secs(24 * 60 * 60),         // 1 dia
+            Schedule::_TwelveHours => Duration::from_secs(12 * 60 * 60),   // 12 horas
+            Schedule::_OneHour => Duration::from_secs(60 * 60),            // 1 hora
+            Schedule::_ThirtyMinutes => Duration::from_secs(30 * 60),      // 30 min
+            Schedule::_OneMinute => Duration::from_secs(60),               // 1 min
         }
     }
 }
